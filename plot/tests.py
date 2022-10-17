07bde48dc90ee5eb7ed2a9db305e007b4b17e2a4
Azim-Kenzh <azimkozho@gmail.com>
2022-10-17 17:05:35 +0600

    add unittest for cultures_fields (method create)

--- plot/tests.py
+++ plot/tests.py
@@ -1,80 +1,12 @@
-# from rest_framework import status_code
-# import json
-#
-# from rest_framework.test import APIRequestFactory, APIClient
-# import os
-# import sys
-# import django
-#
-# django.setup()
-#
-#
-# # Using the standard RequestFactory API to create a form POST request
-# # factory = APIRequestFactory()
-# # request = factory.post('http://127.0.0.1:8111/plots',
-# #                        json.dumps({'user': 1, 'name': 'test_name', 'region': 'test_region'}),
-# #                        content_type='application/json')
-#
-# client = APIClient()
-# request = client.post('/plots/',
-#                        json.dumps({'user': 1, 'name': 'test_name', 'region': 'test_region'}),
-#                        content_type='application/json')
-#
-# print(client.get('/plots/3/').json())
-
-
-
-
-
-
-from rest_framework import status
-from rest_framework.test import APITestCase, URLPatternsTestCase, APIRequestFactory, RequestsClient
-
-# from django.urls import include, path, reverse
-
-
-# class PlotTest(APITestCase, URLPatternsTestCase):
-#     urlpatterns = [
-#         path('', include('config.urls')),
-#     ]
-#
-#     def test_create_plot(self):
-#         url = reverse('plots')
-#         response = self.client.get(url, format='json')
-#         self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-
-# factory = APIRequestFactory()
-# request = factory.post('plots', {'user': 1,'name': 'test_name', 'region': 'test_region'}, format='json')
-
-
-
-
-
-# client = RequestsClient()
-#
-#
-# # Obtain a CSRF token.
-# response = client.get('http://127.0.0.1:8111/')
-# assert response.status_code == 200
-# csrftoken = response.cookies['csrftoken']
-#
-# response = client.post('http://127.0.0.1:8111/plots', json={
-#     'user': 1,
-#     'name': 'test_name',
-#     'region': 'test_region'
-# }, headers={'X-CSRFToken': csrftoken})
-# assert response.status_code == 200
-
-
 from rest_framework.test import APITestCase
-from rest_framework.status import HTTP_200_OK
+from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED
 from django.contrib.auth import get_user_model
 from plot.models import Plot, CultureField
 from .factories import CultureFieldFactory
 
 
 User = get_user_model()
+
 
 class CultureTests(APITestCase):
     def setUp(self):
@@ -88,6 +20,7 @@ class CultureTests(APITestCase):
         self.user = new_user
         self.plot = plot
         self.culture_fields = culture_fields
+
     def test_get_culture_fields_by_user_success_200(self):
         c1 = self.culture_fields[0]
         c2 = self.culture_fields[1]
@@ -115,6 +48,20 @@ class CultureTests(APITestCase):
         response = self.client.get(f"/cultures_fields/{self.user.id}/")
         self.assertEqual(response.data, expected_data, f"Данные не совпадают")
         self.assertEqual(response.status_code, HTTP_200_OK)
+
+
+    def test_create_culture_fields_success_201(self):
+        c1 = self.culture_fields[0]
+        expected_data = {
+                "what": c1.what,
+                "start": c1.start,
+                "end": c1.end,
+                "geometry": '',
+                "plot": self.plot.id
+            }
+        response = self.client.post(f"/cultures_fields/{self.user.id}/", expected_data)
+        print(response.data)
+        self.assertEqual(response.status_code, HTTP_201_CREATED)
 
     def test_get_plots_by_user_success_200(self):
         expected_data = [
@@ -149,6 +96,7 @@ class CultureFactoryTests(APITestCase):
         response = self.client.get(f"/cultures_fields/{c1.plot.user.id}/")
         self.assertEqual(response.data, expected_data, f"Данные не совпадают")
         self.assertEqual(response.status_code, HTTP_200_OK)
+
 
 class PlotTest(APITestCase):
     def test_get_plots_by_not_exist_user_should_be_empty(self):
