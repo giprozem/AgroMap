fb522b285a95b034c63dd4e6c29e3bc1cefbbbbf
Azim-Kenzh <azimkozho@gmail.com>
2022-10-19 16:44:15 +0600

    create factory for material, materialimage and materialblock and create test for material(crud)

---
+++ agrobase/factories.py
@@ -0,0 +1,38 @@
+import factory
+from faker import Faker
+from factory import fuzzy
+
+from agrobase.models import Material, MaterialBlock
+
+
+class MaterialFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Material
+
+    category = 2
+
+
+class MaterialBlockFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = MaterialBlock
+
+    material = factory.SubFactory(MaterialFactory)
+    title = Faker().name()
+    text = Faker().text()
+    image = factory.django.ImageField(color='blue')
+
+
+class MaterialImageFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = MaterialBlock
+
+    material = factory.SubFactory(MaterialFactory)
+    image = factory.django.ImageField(color='blue')
+
+
+
+
+
+
+
+
