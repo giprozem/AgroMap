96cd207269824143f7583f22a188724ad1196711
TIP-ROK <chenye797@gmail.com>
2022-11-28 21:16:14 +0600

    change leaflet settings

--- config/settings.py
+++ config/settings.py
@@ -127,11 +127,10 @@ LEAFLET_CONFIG = {
 
     "SCALE": 'both',
 
-    # "ATTRIBUTION_PREFIX": "Leaflet"
-
-    'TILES': [('OSM', 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
+    'TILES': [('Google', 'http://{s}.google.com/vt/lyrs=s,m,p&x={x}&y={y}&z={z}',
+               {'maxZoom': 20,'subdomains':['mt0','mt1','mt2','mt3']}),
+              ('OSM', 'http://{s}.tile.osm.org/{z}/{x}/{y}.png',
                {'attribution': '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'}),
-              ('Google', 'http://{s}.google.com/vt/lyrs=s,m,p&x={x}&y={y}&z={z}',
-               {'maxZoom': 20,'subdomains':['mt0','mt1','mt2','mt3']})]
+              ]
 
 }
