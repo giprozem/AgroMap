4ed7f7be31277d356a357cf34e449d2fd5bad2b2
sanarip <azimkozho@gmail.com>
2022-12-26 14:11:20 +0600

    adding a fields to culture

--- gip/models/culture.py
+++ gip/models/culture.py
@@ -8,6 +8,8 @@ class Culture(BaseModel):
     name = models.CharField(max_length=55, verbose_name="Культура")
     coefficient_crop = models.FloatField(verbose_name="Коеффициент урожайности")
     history = HistoricalRecords()
+    fill_color = models.CharField(max_length=55, default='#3388FF')
+    stroke_color = models.CharField(max_length=55, default='#3388FF')
 
     def __str__(self):
         return self.name
