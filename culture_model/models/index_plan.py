fd4aae605307f3f76e63c59451c492ba6be9603a
sanarip <azimkozho@gmail.com>
2023-01-05 17:46:38 +0600

    add verbose_name in app culture_model

--- culture_model/models/index_plan.py
+++ culture_model/models/index_plan.py
@@ -6,9 +6,16 @@ from gip.models.region import Region
 
 
 class IndexPlan(models.Model):
-    culture = models.ForeignKey(Culture, on_delete=models.CASCADE, related_name='index_plans')
-    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='index_plans')
-    index = models.ForeignKey(Index, on_delete=models.CASCADE, related_name='index_plans')
-    decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='index_plans')
-    phase = models.ForeignKey(Phase, on_delete=models.CASCADE, related_name='index_plans')
-    value = models.DecimalField(max_digits=5, decimal_places=3, default=0)
+    culture = models.ForeignKey(Culture, on_delete=models.CASCADE, related_name='index_plans', verbose_name='культуа')
+    region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='index_plans', verbose_name='область')
+    index = models.ForeignKey(Index, on_delete=models.CASCADE, related_name='index_plans', verbose_name='индекс')
+    decade = models.ForeignKey(Decade, on_delete=models.CASCADE, related_name='index_plans', verbose_name='декада')
+    phase = models.ForeignKey(Phase, on_delete=models.CASCADE, related_name='index_plans', verbose_name='фаза')
+    value = models.DecimalField(max_digits=5, decimal_places=3, default=0, verbose_name='значение индекса')
+
+    class Meta:
+        verbose_name = 'Плановое значение индекса'
+        verbose_name_plural = "плановые значение индексов"
+
+    def __str__(self):
+        return f"значение индекса ({self.value})"
--- culture_model/models/phase.py
+++ culture_model/models/phase.py
@@ -2,4 +2,11 @@ from django.contrib.gis.db import models
 
 
 class Phase(models.Model):
-    name = models.CharField(max_length=125)
+    name = models.CharField(max_length=125, verbose_name='Название')
+
+    class Meta:
+        verbose_name = 'Фаза развития'
+        verbose_name_plural = "Фазы развития"
+
+    def __str__(self):
+        return self.name
