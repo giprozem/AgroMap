b55a6ca6c5ab789c2386b36178e51596869d47ec
TIP-ROK <chenye797@gmail.com>
2023-01-11 11:15:38 +0600

    added logic for calculating NDMI index

--- indexes/admin.py
+++ indexes/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from django.utils.safestring import mark_safe
 from simple_history.admin import SimpleHistoryAdmin
 
-from indexes.models import NDVIIndex, IndexFact, SatelliteImages
+from indexes.models import NDVIIndex, IndexFact, SatelliteImages, IndexMeaning
+from indexes.forms import IndexMeaningForm
 
 
 @admin.register(NDVIIndex)
@@ -26,15 +27,28 @@ class NDVIAdmin(SimpleHistoryAdmin):
 
 @admin.register(IndexFact)
 class IndexFactAdmin(admin.ModelAdmin):
-    list_display = ('id', 'index_image', 'average_value', 'decade', 'index', 'contour', 'source', 'get_html_photo', )
-    readonly_fields = ('id', 'index_image', 'average_value', 'get_html_photo', )
-    list_display_links = ('id',)
+    list_display = ('id', 'average_value', 'get_description', 'decade', 'index', 'contour', 'source', 'get_html_photo', )
+    readonly_fields = ('id', 'average_value', 'get_html_photo', )
+    list_display_links = ('id', 'get_description', )
 
     def get_html_photo(self, object):
         if object.index_image:
             return mark_safe(f"<img src='{object.index_image.url}' width=100>")
 
+    get_html_photo.short_description = 'Визуализация NDVI'
+
+    def get_description(self, obj):
+        return obj.meaning_of_average_value.description
+
+    get_description.short_description = 'Значение показателя индекса'
+
 
 @admin.register(SatelliteImages)
 class SatelliteImagesAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(IndexMeaning)
+class IndexMeaningAdmin(admin.ModelAdmin):
+    form = IndexMeaningForm
+    # pass
