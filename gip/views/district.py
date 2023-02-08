b62ae6684153a4cd661e7cdd1089a1da047fe9b8
sanarip <azimkozho@gmail.com>
2023-02-08 19:54:14 +0600

    add filter (region) in api district

--- gip/views/district.py
+++ gip/views/district.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from gip.models import District
@@ -7,3 +8,5 @@ from gip.serializers.district import DistrictSerializer
 class DistrictViewSet(viewsets.ModelViewSet):
     queryset = District.objects.all()
     serializer_class = DistrictSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['region',]
