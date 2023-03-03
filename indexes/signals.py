34a261a6ccd24db31b67bbed09b6a2b67fe38a10
TIP-ROK <chenye797@gmail.com>
2023-03-03 10:27:29 +0600

    removed signal

--- indexes/signals.py
+++ indexes/signals.py
@@ -5,12 +5,4 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from indexes.models.satelliteimage import SatelliteImages
-from indexes.utils import creating_indexes
 
-
-@receiver(post_save, sender=SatelliteImages)
-def creating_index_signal(sender, instance, created, **kwargs):
-    time.sleep(60)
-    if created:
-        thread_object = Thread(target=creating_indexes, args=(instance.date, instance))
-        thread_object.start()
