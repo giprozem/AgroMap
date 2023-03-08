e74da750c63d43a5c74fadb76498df048c6ab3e6
TIP-ROK <chenye797@gmail.com>
2023-03-08 12:35:51 +0600

    replaced urls of gip app and indexes app

--- config/urls.py
+++ config/urls.py
@@ -5,20 +5,10 @@ from django.urls import path, include
 from django.utils.translation import gettext_lazy as _
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
-from rest_framework.routers import DefaultRouter
 from schema_graph.views import Schema
 
 from account.views.authenticated import LoginAgromapView
 from config import settings
-from gip.views.geoserver import Geoserver
-from hub.views.authetificated import LoginHubView
-from hub.views.zem_balance_api import ZemBalanceViewSet
-
-# router = DefaultRouter()
-#
-# #Zem Balance
-# router.register('zem_balance', ZemBalanceViewSet, basename='zem_balance')
-
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -31,12 +21,11 @@ schema_view = get_schema_view(
 urlpatterns = [
     path("i18n/", include("django.conf.urls.i18n")),
     path('hub/', include("hub.urls")),
-    path('', include('indexes.urls')),
-    path('', include('gip.urls')),
+    path('veg/', include('indexes.urls')),
+    path('gip/', include('gip.urls')),
     path('docs/', schema_view.with_ui()),
     path("schema/", Schema.as_view()),
     path('login_agromap/', LoginAgromapView.as_view()),
-    path('geoserver/', Geoserver.as_view()),
 ]
 
 urlpatterns += i18n_patterns(
