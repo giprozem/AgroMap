37cea040320be623d0fee06f0e9e52b6252b7446
Azim-Kenzh <azimkozho@gmail.com>
2023-03-11 15:46:19 +0600

    add swagger test for api and preparing geoserver crontab

--- hub/views/authetificated.py
+++ hub/views/authetificated.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import authenticate
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import exceptions
@@ -7,6 +8,9 @@ from rest_framework.authtoken.models import Token
 
 
 class LoginHubView(APIView):
+    @swagger_auto_schema(
+        operation_summary='do not required for front'
+    )
     def post(self, request, *args, **kwargs):
         serializer = LoginSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
