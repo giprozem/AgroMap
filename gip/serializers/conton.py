8d2b45430fd0a8936a1268ea9df253564485f51f
Almaz <sh.almaz999@mail.ru>
2023-03-17 15:13:26 +0600

    adding region to contonserializer

--- gip/serializers/conton.py
+++ gip/serializers/conton.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
-from gip.models import Conton, Region, District
-from gip.serializers.region import RegionWithoutPolygonSerializer
+from gip.models import Conton, Region
 
 
 class ContonSerializer(serializers.ModelSerializer):
+    region = serializers.SerializerMethodField()
+
+    def get_region(self, obj):
+        region = Region.objects.get(name=obj.district.region)
+        return region.pk
+
     class Meta:
         model = Conton
         exclude = ('name', )
