19c71a89abddd76bee154a6063add20a20d296d6
TIP-ROK <chenye797@gmail.com>
2023-03-28 12:18:39 +0600

    added blank=True, null=True temporarry for district

--- gip/models/district.py
+++ gip/models/district.py
@@ -9,7 +9,7 @@ class District(BaseModel):
     code_soato = models.CharField(max_length=30, unique=True, null=True, blank=True, verbose_name=_("Код СОАТО"))
     region = models.ForeignKey(Region, on_delete=models.CASCADE, related_name='districts', verbose_name=_("Область"))
     name = models.CharField(max_length=55, verbose_name=_("Район"))
-    polygon = models.GeometryField(geography='Kyrgyzstan', verbose_name=_("Контур"))
+    polygon = models.GeometryField(geography='Kyrgyzstan', verbose_name=_("Контур"), blank=True, null=True)
 
     def __str__(self):
         return self.name
