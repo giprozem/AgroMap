167f2a929de4eb62228d1519a2c3ec31fcd9a0bd
TIP-ROK <chenye797@gmail.com>
2023-03-28 11:02:03 +0600

    added API to send data for training productivity

--- indexes/serializers/statistics_veg_index.py
+++ indexes/serializers/statistics_veg_index.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 
+from gip.models import Contour
 from indexes.models.actual_veg_index import ActualVegIndex
 
 
@@ -11,15 +12,15 @@ class VegIndexSerializer(serializers.ModelSerializer):
         fields = ('index', 'average_value', )
 
 
-# class ContourYearStatisticsSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = ContourYear
-#         exclude = ('polygon', 'created_at', 'updated_at', )
-#
-#     def to_representation(self, instance):
-#         representation = super().to_representation(instance)
-#         result = VegIndexSerializer(instance.actual_veg_index.all(), many=True).data
-#         for i in result:
-#             representation[f'{i["index"]}'] = i['average_value']
-#
-#         return representation
+class ContourStatisticsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contour
+        exclude = ('polygon', 'created_at', 'updated_at', )
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        result = VegIndexSerializer(instance.actual_veg_index.all(), many=True).data
+        for i in result:
+            representation[f'{i["index"]}'] = i['average_value']
+
+        return representation
