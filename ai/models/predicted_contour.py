101bab3d7673879fcfd042703aa959c6aa079750
Azim-Kenzh <azimkozho@gmail.com>
2023-03-28 13:33:34 +0600

    changed name class and model adjustment ai

--- ai/models/predicted_contour.py
+++ ai/models/predicted_contour.py
@@ -2,19 +2,21 @@ from django.contrib.gis.db import models
 from gip.models.conton import Conton
 from gip.models.district import District
 
-class Images(models.Model):
-    image = models.ImageField(upload_to='media/', blank=True)
+
+class Images_AI(models.Model):
+    image = models.ImageField(upload_to='images_ai', blank=True)
 
 
-class Contour(models.Model):
+class Contour_AI(models.Model):
     polygon = models.GeometryField(geography='Kyrgyzstan', blank=True, null=True)
-    percent = models.FloatField(null=True, default=0.5)
-    culture = models.CharField(max_length=50, blank=True)
-    image = models.ForeignKey(Images, on_delete=models.PROTECT, related_name='images')
-    conton = models.ForeignKey(Conton, on_delete=models.PROTECT, related_name='canton', null=True)
-    district = models.ForeignKey(District, on_delete=models.PROTECT, related_name='district', default=1)
+    percent = models.FloatField(null=True, blank=True)
+    culture = models.CharField(max_length=50, blank=True, null=True)
+    image = models.ForeignKey(Images_AI, on_delete=models.SET_NULL, related_name='contour_ai', blank=True, null=True)
+    conton = models.ForeignKey(Conton, on_delete=models.SET_NULL, related_name='contour_ai', null=True, blank=True)
+    district = models.ForeignKey(District, on_delete=models.SET_NULL, related_name='contour_ai', null=True,
+                                 blank=True)
     productivity = models.CharField(max_length=20, blank=True)
 
 
 class Yolo(models.Model):
-    ai = models.FileField(upload_to='models/')
+    ai = models.FileField(upload_to='models_ai/')
