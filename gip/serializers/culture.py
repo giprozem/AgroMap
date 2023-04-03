3fcbde17f240430df9ec2010c7152f7c101f2746
TIP-ROK <chenye797@gmail.com>
2023-04-03 20:17:40 +0600

    PolygonsInScreen do not return deleted obj, removed unused culture fields, returned is authenticated to culture and AuthDetailContourViewSet

--- gip/serializers/culture.py
+++ gip/serializers/culture.py
@@ -6,4 +6,4 @@ from gip.models import Culture
 class CultureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Culture
-        exclude = ('name',)
+        exclude = ('name', 'created_at', 'updated_at', )
