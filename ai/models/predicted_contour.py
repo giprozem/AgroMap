732fd811f194f8890bb98f024b782f10dcd080b8
TIP-ROK <chenye797@gmail.com>
2023-04-04 15:43:36 +0600

    added extra fields, added signal for Contour_AI

--- ai/models/predicted_contour.py
+++ ai/models/predicted_contour.py
@@ -1,6 +1,8 @@
 from django.contrib.gis.db import models
+from django.utils.translation import gettext_lazy as _
 from solo.models import SingletonModel
 
+from gip.models import LandType, Culture, SoilClass
 from gip.models.conton import Conton
 from gip.models.district import District
 
@@ -10,14 +12,34 @@ class Images_AI(models.Model):
 
 
 class Contour_AI(models.Model):
-    polygon = models.GeometryField(geography='Kyrgyzstan', blank=True, null=True)
-    percent = models.FloatField(null=True, blank=True)
+    conton = models.ForeignKey(Conton, on_delete=models.SET_NULL, related_name='contour_ai', null=True, blank=True,
+                               verbose_name=_("Округ"))
+    type = models.ForeignKey(
+        LandType,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name=_("Тип земли"),
+        related_name='ai_contours'
+    )
+    polygon = models.GeometryField(geography='Kyrgyzstan', verbose_name=_("Контур"), blank=True, null=True)
+    year = models.CharField(max_length=20, verbose_name=_("Год"), null=True, blank=True)
+    productivity = models.CharField(max_length=20, blank=True, verbose_name=_("Продуктивность"))
+    area_ha = models.FloatField(blank=True, null=True, verbose_name=_("Площадь в гектарах"))
+    is_deleted = models.BooleanField(default=False, verbose_name=_('Удаленный'))
     culture = models.CharField(max_length=50, blank=True, null=True)
+    elevation = models.CharField(max_length=25, blank=True, null=True, verbose_name=_('Высота'))
+    soil_class = models.ForeignKey(SoilClass, on_delete=models.SET_NULL, blank=True, null=True,
+                                   verbose_name='Тип почвы')
+    percent = models.FloatField(null=True, blank=True)
     image = models.ForeignKey(Images_AI, on_delete=models.SET_NULL, related_name='contour_ai', blank=True, null=True)
-    conton = models.ForeignKey(Conton, on_delete=models.SET_NULL, related_name='contour_ai', null=True, blank=True)
-    district = models.ForeignKey(District, on_delete=models.SET_NULL, related_name='contour_ai', null=True,
-                                 blank=True)
-    productivity = models.CharField(max_length=20, blank=True)
+
+    district = models.ForeignKey(
+        District,
+        on_delete=models.SET_NULL,
+        related_name='contour_ai',
+        null=True,
+        blank=True
+    )
 
 
 class Yolo(SingletonModel):
---
+++ ai/signals.py
@@ -0,0 +1,68 @@
+from django.contrib.gis.db.models.functions import Area
+from django.db import connection
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from shapely.wkt import loads
+import requests
+from django.contrib.gis.geos import Point
+from gip.models import Elevation
+from ai.models.predicted_contour import Contour_AI
+
+
+@receiver(post_save, sender=Contour_AI)
+def update(sender, instance, created, **kwargs):
+    if created:
+        geom = Contour_AI.objects.annotate(area_=Area("polygon")).get(id=instance.id)
+        with connection.cursor() as cursor:
+            cursor.execute(f"""
+            SELECT subquery.name, subquery.id
+            FROM (
+                SELECT ST_Area(ST_Intersection(scm.polygon::geometry,
+                '{geom.polygon}'::geography::geometry)) / ST_Area(scm.polygon::geometry) * 100 as percent,
+                    sc.id, sc.name
+                FROM gip_soilclassmap as scm
+                JOIN gip_soilclass as sc
+                ON sc.id = scm.soil_class_id
+            ) as subquery
+            WHERE subquery.percent > 1
+            ORDER BY subquery.percent DESC
+            LIMIT 1;
+                    """)
+            rows = cursor.fetchall()
+            print(rows)
+            result_soil_class = rows[0][1] if rows != [] else None
+        instance.soil_class_id = result_soil_class
+        center = loads(f"{geom.polygon.centroid}".strip('SRID=4326;'))
+        x, y = center.x, center.y
+        result_elevation = \
+            requests.get(f"https://api.opentopodata.org/v1/gebco2020?locations={y},{x}").json()
+        if result_elevation:
+            results = result_elevation['results'][0]['elevation']
+            Elevation.objects.create(point=Point(x, y), elevation=results)
+            instance.elevation = results
+            instance.save()
+        ha = round(geom.area_.sq_km * 100, 2)
+        instance.area_ha = ha
+        instance.save()
+    else:
+        geom = Contour_AI.objects.annotate(area_=Area("polygon")).get(id=instance.id)
+        with connection.cursor() as cursor:
+
+            cursor.execute(f"""
+            SELECT subquery.name, subquery.id
+            FROM (
+                SELECT ST_Area(ST_Intersection(scm.polygon::geometry,
+                '{geom.polygon}'::geography::geometry)) / ST_Area(scm.polygon::geometry) * 100 as percent,
+                sc.id, sc.name
+                FROM gip_soilclassmap as scm
+                JOIN gip_soilclass as sc
+                ON sc.id = scm.soil_class_id
+            ) as subquery
+            WHERE subquery.percent > 1
+            ORDER BY subquery.percent DESC
+            LIMIT 1;
+                        """)
+            rows = cursor.fetchall()
+            result_soil_class = rows[0][1] if rows != [] else None
+        ha = round(geom.area_.sq_km * 100, 2)
+        Contour_AI.objects.filter(id=instance.id).update(area_ha=ha, soil_class_id=result_soil_class)
