3e9777253859ecea2e47a4587b0fb8721d5e21b5
Almaz <sh.almaz999@mail.ru>
2023-04-10 20:26:28 +0600

    translating ai and hub appname

--- ai/apps.py
+++ ai/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class AiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ai'
+    verbose_name = _('ИИ')
 
     def ready(self):
         import ai.signals
--- hub/apps.py
+++ hub/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class HubConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'hub'
+    verbose_name = _('Эталонная база данных Гипрозем')
