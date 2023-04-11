4c5dafefb2a55b273b4979230fb558301f4db8e7
TIP-ROK <chenye797@gmail.com>
2023-04-11 18:42:19 +0600

    added response to cron_ai_geoserver_run

--- scripts/cron_ai_geoserver.py
+++ scripts/cron_ai_geoserver.py
@@ -12,8 +12,10 @@ def run():
     with connection.cursor() as cursor:
         cursor.execute(f"""
                         SELECT cntr.id, cntr.district_id, cntr.percent, cntr.culture_id, rgn.id as rgn,
+                        cntr.type_id AS land_type_id, cntr.year, cntr.area_ha, cntr.productivity,
                         St_AsGeoJSON(cntr.polygon) as polygon
                         FROM ai_contour_ai as cntr
+                        JOIN gip_landtype AS land ON land.id=cntr.type_id
                         JOIN gip_district AS dst ON dst.id=cntr.district_id
                         JOIN gip_region AS rgn ON rgn.id=dst.region_id
                         WHERE cntr.id > 11016;
@@ -21,14 +23,20 @@ def run():
                        )
         rows = cursor.fetchall()
         data = []
+
         for i in rows:
             data.append({"type": "Feature", "properties": {
-                'id': i[0],
-                'dst': i[1],
-                'prt': i[2],
-                'clt': i[3],
-                'rgn': i[4]},
-                "geometry": eval(i[-1])
+                    'id': i[0],
+                    'dst': i[1],
+                    'prt': i[2],
+                    'clt': i[3],
+                    'rgn': i[4],
+                    'ltype': i[5],
+                    'year': i[6],
+                    'area': i[7],
+                    'prdvty': i[8]
+                    },
+                         "geometry": eval(i[-1])
                          }
                         )
 
