a667b43e12122509d04a895042c727970a1f7156
TIP-ROK <chenye797@gmail.com>
2023-04-12 21:41:55 +0600

    comited permission

--- gip/views/culture.py
+++ gip/views/culture.py
@@ -8,9 +8,9 @@ class CultureViewSet(ModelViewSet):
     queryset = Culture.objects.all()
     serializer_class = CultureSerializer
 
-    def get_permissions(self):
-        if self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy':
-            permission_classes = [IsAdminUser]
-        else:
-            permission_classes = [IsAuthenticated]
-        return [permission() for permission in permission_classes]
+    # def get_permissions(self):
+    #     if self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy':
+    #         permission_classes = [IsAdminUser]
+    #     else:
+    #         permission_classes = [IsAuthenticated]
+    #     return [permission() for permission in permission_classes]
