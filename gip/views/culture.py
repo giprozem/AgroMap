a3eb7772de2ff64b2239fd50ab7fe231e39a2df7
Almaz <sh.almaz999@mail.ru>
2023-04-13 10:39:00 +0600

    culture permissions

--- gip/views/culture.py
+++ gip/views/culture.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from gip.models.culture import Culture
 from gip.serializers.culture import CultureSerializer
 
@@ -8,9 +8,11 @@ class CultureViewSet(ModelViewSet):
     queryset = Culture.objects.all()
     serializer_class = CultureSerializer
 
-    # def get_permissions(self):
-    #     if self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy':
-    #         permission_classes = [IsAdminUser]
-    #     else:
-    #         permission_classes = [IsAuthenticated]
-    #     return [permission() for permission in permission_classes]
+    def get_permissions(self):
+        if self.action == 'update' or self.action == 'partial_update' or self.action == 'destroy':
+            permission_classes = [IsAdminUser]
+        elif self.action == 'create':
+            permission_classes = [IsAuthenticated]
+        else:
+            permission_classes = [AllowAny]
+        return [permission() for permission in permission_classes]
