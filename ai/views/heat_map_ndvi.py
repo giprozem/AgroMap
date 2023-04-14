347e75eff20bb66b3822ff95a4b385bfbcee2a75
TIP-ROK <chenye797@gmail.com>
2023-04-14 10:30:12 +0600

    added script for heat map without auto upload to geoserver

---
+++ ai/views/heat_map_ndvi.py
@@ -0,0 +1,17 @@
+from threading import Thread
+
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from scripts.heat_map_ndvi import run
+
+
+class HeatMapAPIView(APIView):
+    @swagger_auto_schema(
+        operation_summary='do not required for front'
+    )
+    def get(self, request, *args, **kwargs):
+        thread_object = Thread(target=run)
+        thread_object.start()
+        return Response('process is started', status=200)
