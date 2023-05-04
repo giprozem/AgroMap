f23844dfbf821906a2a175d1f52f8babd524f6bc
Azim-Kenzh <azimkozho@gmail.com>
2023-05-04 18:25:03 +0600

    fix

--- ai/views/predict_culture.py
+++ ai/views/predict_culture.py
@@ -5,7 +5,7 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ai.utils.predicting_culture import predicting_culture, predict_culture
+from ai.utils.predicting_culture import predicting_culture
 
 
 class CulturePredict(APIView):
