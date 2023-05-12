6432944f1d1817ad51de8bee6f5527c83d6a3eda
Azim-Kenzh <azimkozho@gmail.com>
2023-05-12 11:46:12 +0600

    added multithreading to create indexes, according to the characteristics of the server

--- indexes/views/generated_indexes.py
+++ indexes/views/generated_indexes.py
@@ -1,9 +1,7 @@
-from threading import Thread
-
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from indexes.utils import veg_index_creating
+from indexes.utils import create_veg_indexes
 from drf_yasg.utils import swagger_auto_schema
 
 
@@ -12,6 +10,5 @@ class CreatingVegIndexesAPIView(APIView):
         operation_summary='do not required for front'
     )
     def get(self, request):
-        thread_object = Thread(target=veg_index_creating)
-        thread_object.start()
+        create_veg_indexes()
         return Response('Ok')
