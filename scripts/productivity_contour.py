87c1abebbea840fc11b70667f0eb8d112c6d8ca7
Azim-Kenzh <azimkozho@gmail.com>
2023-05-15 17:24:51 +0600

    fix bug

--- scripts/productivity_contour.py
+++ scripts/productivity_contour.py
@@ -10,7 +10,7 @@ def run():
         type_id=2,
         actual_veg_index__index_id=1,
         actual_veg_index__date__range=('2022-04-01', '2022-11-01')
-    ).annotate(avg_value=Avg('actual_veg_index__average_value')).count()
+    ).annotate(avg_value=Avg('actual_veg_index__average_value'))
 
     with open(f'K_Nearest_Neighbors_Regressor_model_65.pkl', 'rb') as f:
         model = pickle.load(f)
@@ -18,6 +18,6 @@ def run():
     for contour in contours:
         Contour.objects.filter(id=contour.id).update(
             predicted_productivity=round(
-                model.predict([[contour.avg_value, contour.elevation, contour.soil_class_id]][0], 2)
+                model.predict([[contour.avg_value, contour.elevation, contour.soil_class_id]])[0], 2
             ),
         )
