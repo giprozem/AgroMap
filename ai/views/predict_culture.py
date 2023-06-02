d26d51f19dbee916f6d0e5cf7d30083f0f768d0c
TIP-ROK <chenye797@gmail.com>
2023-06-02 14:53:34 +0600

    removed unused scripts

--- ai/views/predict_culture.py
+++ ai/views/predict_culture.py
@@ -5,19 +5,6 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from ai.utils.predicting_culture import predicting_culture
-
-
-class CulturePredict(APIView):
-    @swagger_auto_schema(
-        operation_summary='do not required for front',
-        operation_description='predicting culture in given satellite image id'
-    )
-    def get(self, request, *args, **kwargs):
-        thread_obj = Thread(target=predicting_culture, args=(request.query_params['satellite_id'],))
-        thread_obj.start()
-        return Response('result', status=200)
-
 
 class PivotTableCulture(APIView):
     def get(self, request):
